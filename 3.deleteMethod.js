0a9b3f6b7b1a76656142ac1da0adeac59a9987e4
abhishekmardiya <mardiyaabhishek@gmail.com>
2022-07-14 12:12:52 +0530

    fetch-methods-added

---
+++ 3.deleteMethod.js
@@ -0,0 +1,16 @@
+fetch("https://reqres.in/api/users/2", {
+  method: "DELETE",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+let deleteData = async () => {
+  await fetch("https://reqres.in/api/users/2", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+deleteData()
